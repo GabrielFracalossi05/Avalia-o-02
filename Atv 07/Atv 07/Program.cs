b56ea3584c7c920db369fcf14dc190d6a7afeb3a
Gabriel Fracalossi <0064211@SESI-ES.ORG.BR>
2025-10-21 09:02:08 -0300

    Atividade 07 Finalizada

---
+++ Atv 07/Atv 07/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.Write("Digite o número inicial (Start): ");
+int start = int.Parse(Console.ReadLine());
+Console.Write("Digite o número final (End): ");
+int end = int.Parse(Console.ReadLine());
+
+for (int num = start; num <= end; num++)
+
+{
+
+    Console.WriteLine($"\nTabuada do {num}:");
+
+    for (int i = 1; i <= 10; i++)
+
+    {
+
+        int resultado = num * i;
+
+        Console.WriteLine($"{num} x {i} = {resultado}");
+
+    }
+
+}
+    
+
+
+
